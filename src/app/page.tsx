89dc0442762efa0fa9c6b5958e5bf19b6663727b
Samuel <samuel.viana19@aluno.ce.gov.br>
2026-06-09 19:04:44 -0300

    Primeiro Commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/common/header/pages"
+
+export default function Home() {
+  return (
+    <div>
+      <Header />
+      <main className="p-8">
+      </main>
+    </div>
+  )
+}
---
+++ src/components/common/header/pages.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link"
+
+export default function Header() {  
+  return (
+    <header className="bg-blue-950 flex justify-between h-20">
+      <div className="text-blue-200 text-2xl p-8 pl-7">
+        <h1 className="font-sans">Aluno-ON</h1>
+      </div>
+
+      <ul className="flex gap-8 mx-left p-10 pl-6 text-blue-200">
+        <li>
+          <Link href="/cadastro" className="hover:text-cyan-200 hover:underline transition-all">
+            Cadastro
+          </Link>
+        </li>
+        <li>
+          <Link href="/auth/login" className="hover:text-cyan-200 hover:underline transition-all">
+            Login
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
+}
+
