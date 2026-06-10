c97931d88b6ecf23f0a69535d5df1c39df6ce7e3
Samuel <samuel.viana19@aluno.ce.gov.br>
2026-06-10 20:49:31 -0300

    adiciona hero com botão de login

---
+++ my-app/src/app/components/common/hero/page.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link"
+export default function Hero() {
+    return (
+        <div className="min-h-screen flex items-center justify-center">
+            <main className="flex flex-col items center gap-6 text-center">
+
+                <h1 className="text-blue-500 font-semibold">Bem Vindo ao Aluno On</h1>
+
+                <p className="text-blue-300">Acompanhe sua carreira Estudantil aqui!</p>
+             <Link
+                href="/auth/login"
+                className="bg-cyan-200 text-blue-400 font-semibold text-sm px-6 py-3"
+                    >  
+                    Entrar
+                </Link>
+            </main>
+        </div>
+    );
+}
--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "@/src/app/components/common/header/pages"
+import Hero from "@/src/app/components/common/hero/page"
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <Hero />
       <main className="p-8">
       </main>
     </div>
