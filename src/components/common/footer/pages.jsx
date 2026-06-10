bdb16c1ea3e0e17f8aa64ff2b648ff2a1d41bfdb
abel maia sousa <abel.sousa217@gmail.com>
2026-06-10 15:32:28 -0300

    excluir os arquivos fudidos que o pessoal dublicou de novo

--- src/components/common/footer/pages.jsx
+++
@@ -1,9 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-blue-950 text-2x1 align-middle">
-            <div>
-                <h1 className="text-blue-200">© 2026 Aluno-ON. Todos os direitos reservados.</h1>
-            </div>
-        </footer>
-    )
-}
